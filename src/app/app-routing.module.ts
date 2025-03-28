7535effb90ca8c6bb5c66f2b6d9c6a884c272cf0
Prakriti <pbiswas@atworkcare.ca>
2025-03-27 23:14:42 -0400

    Component creation
    Created the basic components and did a template for Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InstructorComponent } from './Components/instructor/instructor.component';
+import { HomeComponent } from './Components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/instructor', pathMatch: 'full' },
+  { path: '', component:HomeComponent },
   { path: 'instructor', component: InstructorComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 import { InstructorComponent } from './Components/instructor/instructor.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { VerificationComponent } from './Components/verification/verification.component';
+import { HomeComponent } from './Components/home/home.component';
+import { StudentComponent } from './Components/student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InstructorComponent
+    InstructorComponent,
+    LoginComponent,
+    SignupComponent,
+    VerificationComponent,
+    HomeComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
